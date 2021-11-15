05df69d233be0f63497dbe592e2fb5233ad27927
Giadaspt <saputo.giada@gmail.com>
2021-11-15 21:39:42 +0100

    layout completed

--- milestone 1/js/script.js
+++ milestone 1/js/script.js
@@ -2,4 +2,207 @@ const root = new Vue({
 
   el: '#root',
 
+  data: {
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: 'img/avatar_1.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '10.50 15/11/2021',
+            text: 'Hai portato a spasso il cane?',
+            status: 'sent'
+          },
+          {
+            date: '10.50 15/11/2021',
+            text: 'Ricordati di dargli da mangiare',
+            status: 'sent'
+          },
+          {
+            date: '10.50 15/11/2021',
+            text: 'Tutto fatto!',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '10.50 15/11/2021',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Angela',
+        avatar: '_5',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Lino',
+        avatar: '_6',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Patrizio',
+        avatar: '_7',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Simone',
+        avatar: '_8',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+      {
+        name: 'Matteo',
+        avatar: '_9',
+        visible: true,
+        messages: [
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'sent'
+          },
+          {
+            date: '',
+            text: '',
+            status: 'received'
+          },
+        ],
+      },
+    ]
+  }
+
 });
